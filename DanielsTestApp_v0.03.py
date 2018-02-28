88f7407e32e5974caaa391989627be54d254f557
Daniel Schuette <d.schuette@online.de>
2018-02-28 18:28:16 -0500

    v0.2 of VersionCheck moduleintroduced

--- DanielsTestApp_v0.03.py
+++ DanielsTestApp_v0.03.py
@@ -5,13 +5,7 @@ latest version: v0.03 (as of 02/28/2018)
 -> runs with python 2.7.14 and python 3.6.x
 '''
 
-# identify python version and tinkter that should be used
-import sys
-if sys.version_info[0] < 3:
-    print("You're running Python 2.x")
-    import Tkinter as tk
-else:
-    print("You're running Python 3.x")
-    import tkinter as tk
+import VersionCheck as vs
 
-    
+vs.pythonVersionCheck()
+
--- VersionCheck.py
+++ VersionCheck.py
@@ -1,9 +1,9 @@
 # identify python version and import functions that are version dependent
 def pythonVersionCheck():
 	import sys
-	if sys.version_info[0] < 3:
-    	print("You're running Python 2.x")
-    	import Tkinter as tk
+	if sys.version < 3:
+		print("You're running Python 2.x")
+		import TKinter as tk
 	else:
-    	print("You're running Python 3.x")
-    	import tkinter as tk
+		print("You're running Python 2.x")
+		import tkinter as tk
