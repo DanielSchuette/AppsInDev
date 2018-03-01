883548423a0fd2e025c1ef8c2cf0f7ce0f9d73e1
Daniel Schuette <d.schuette@online.de>
2018-02-28 19:33:08 -0500

    'tk' works now

--- DanielsTestApp_v0.03.py
+++ DanielsTestApp_v0.03.py
@@ -4,8 +4,14 @@ This is a test .app to test tkinter and py2app
 latest version: v0.03 (as of 02/28/2018)
 -> runs with python 2.7.14 and python 3.6.x
 '''
-
+# test which version runs to install Tkinter correctly
 import VersionCheck as vs
+vs.pythonVersionCheck() # makes tk available in module
+from VersionCheck import tk # imports tk from module
 
-vs.pythonVersionCheck()
+# initializes a window
+root = tk.Tk()
+
+# starts mainloop
+root.mainloop() 
 
--- VersionCheck.py
+++ VersionCheck.py
@@ -1,9 +1,13 @@
 # identify python version and import functions that are version dependent
 def pythonVersionCheck():
-	import sys
-	if sys.version < 3:
+	import sys # sys is not a global import
+	global tk # ... to ensure that tk is globally available IN THIS MODULE
+	print("\n" + "********************************" + "\n")
+	print("Program started successfully")
+	if sys.version_info[0] < 3:
 		print("You're running Python 2.x")
-		import TKinter as tk
+		tk = __import__("Tkinter", globals(), locals())
 	else:
-		print("You're running Python 2.x")
-		import tkinter as tk
+		print("You're running Python 3.x")
+		tk = __import__("Tkinter", globals(), locals())
+	print("\n" + "********************************" + "\n")
